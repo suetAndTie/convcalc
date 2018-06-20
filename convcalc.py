f6870e9ea12e17b1fb1d14b3d07e67ed02a372d3
suetAndTie <trtets@gmail.com>
2018-06-20 13:50:33 -0700

    updated options

--- convcalc.py
+++ convcalc.py
@@ -10,13 +10,17 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', type=int, required=True, help='Input size')
-parser.add_argument('--size', type=int, required=True, help='Kernel Size')
+# Required arguments
+parser.add_argument('input', metavar='input_size', type=int, help='Image input size')
+parser.add_argument('kernel', metavar='kernel_size', type=int, help='Kernel size')
+
+# Optional arguments
 parser.add_argument('--stride', type=int, default=1, help='Stride of convolution')
 parser.add_argument('--pad', type=int, default=0, help='0-padding')
 
+# Print out
 opt = parser.parse_args()
 print(opt)
-output_size = ((opt.input - opt.size + 2 * opt.pad) // opt.stride) + 1
+output_size = ((opt.input - opt.kernel + 2 * opt.pad) // opt.stride) + 1
 print('Output Size: ' + str(output_size))
 
